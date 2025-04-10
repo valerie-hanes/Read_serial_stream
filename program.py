aca639a3baa3f07a3a16ad9707952e0547cdb218
valerie-hanes <vhanes27@my.whitworth.edu>
2025-04-10 10:38:50 -0700

    save

--- program.py
+++ program.py
@@ -23,7 +23,11 @@ ser = serial.Serial("/dev/cu.usbmodem0010502332261",
         bytesize=serial.EIGHTBITS,
         timeout=2) 
 
-for i  in np.arange(0,10,1):
+start = time.time()
+i=0
+while (time.time()< start+20): #for how long do we want it to run?
+#for i  in np.arange(0,5,1): #how many times do we want it to run?
+
     #read from the microcontroller  (will run forever if there is nothing to read and no timout defined above)
     trash_data = ser.readline()
     data = ser.readline().decode('utf-8').strip().split('\t') # read in the data, stop at an \n, take off junk at the end, split by tab
@@ -31,30 +35,36 @@ for i  in np.arange(0,10,1):
 
 
     data = [float(x) for x in data] #turn each string into a float value
-   
-    #CONTOUR PLOT
-    contour_arr = (np.array(data)) #store the data into an array for the contour plot
-    contour_arr = contour_arr.reshape([10,10]) # change to 10 by 10
-    contour_arr = contour_arr[::-1] #flip contour plot array to make it correspond to the plate
+    for j in np.arange(0,len(data),1):
+        if data[j] <= -666: #account for a sensor that has died
+            data[j] = np.nan
 
-    
-    x = np.arange(0,33+(1.0/3.0),(1.0/3.0)+3) #x values in 10 "rows" on a 30 cm board
-    y=np.arange(0,33+(1.0/3.0),(1.0/3.0)+3) #y values in 10 "columns" on a 30 cm board
-    #print(contour_arr)
-
-    fig, ax = plt.subplots() 
-    c = ax.contourf(x,y,contour_arr,levels=1000,cmap='jet') #setting the contour/color of the plot
-    plt.colorbar(c,label = 'Temperature ($ \degree$C)') #add color bar to plot
-    ax.set_xlabel('$x$-Position (cm)')
-    ax.set_ylabel('$y$-Position (cm)')
-    
 
     #SAVE CSV
     csv_arr = np.array(data) #store the data into an array for saving the csv file
     df = pd.DataFrame(csv_arr) # turn to dataframe
     df.to_csv(str(t)+'.csv',index=False,header=False) #save the data using the time at which it was read
-    if i == 0: #only look at the first contour plot of the loop
+
+    #CONTOUR PLOT
+
+    if i==0: #only look at the first contour plot of the loop to save time
+        
+        contour_arr = (np.array(data)) #store the data into an array for the contour plot
+        contour_arr = contour_arr.reshape([10,10]) # change to 10 by 10
+        contour_arr = contour_arr[::-1] #flip contour plot array to make it correspond to the plate
+
+
+        x = np.arange(0,33+(1.0/3.0),(1.0/3.0)+3) #x values in 10 "rows" on a 30 cm board
+        y=np.arange(0,33+(1.0/3.0),(1.0/3.0)+3) #y values in 10 "columns" on a 30 cm board
+        #print(contour_arr)
+
+        fig, ax = plt.subplots() 
+        c = ax.contourf(x,y,contour_arr,levels=1000,cmap='jet') #setting the contour/color of the plot
+        plt.colorbar(c,label = 'Temperature ($ \degree$C)') #add color bar to plot
+        ax.set_xlabel('$x$-Position (cm)')
+        ax.set_ylabel('$y$-Position (cm)')
         plt.show()
+        i=9
     time.sleep(2) 
 
     
