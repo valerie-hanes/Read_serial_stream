130ba1e1412cde7d2e25887bbf3a58abb51abbdb
valerie-hanes <vhanes27@my.whitworth.edu>
2025-04-10 08:04:30 -0700

    save

--- program.py
+++ program.py
@@ -25,29 +25,29 @@ for i  in np.arange(0,5,1):
 
     #SPLIT STRING
     data = "100,110,15,15,14,12,100,12,12,9,12,12,5,5,4,3,5,5,5,3,4,3,1,2,3,5,4,4,7,5,4,5,4,6,3,100" #delete
-    data = data.split(',')
-    data = [float(x) for x in data]
-    contour_arr = (np.array(data))
+    data = data.split(',') #split the string at each comma
+    data = [float(x) for x in data] #turn each string into a float value
+    
+    contour_arr = (np.array(data)) #store the data into an array for the contour plot
     contour_arr = contour_arr.reshape([6,6]) # change to 10 by 10
-
-    contour_arr = contour_arr[::-1]
+    contour_arr = contour_arr[::-1] #flip contour plot array to make it correspond to the plate
 
     #CONTOUR PLOT
-    x = np.arange(0,36,6) 
-    y=np.arange(0,36,6)
+    x = np.arange(0,36,6) #x values in intervals of 5 stopping at 30
+    y=np.arange(0,36,6) #y values in intervals of 5 stopping at 30
     #print(contour_arr)
 
-    fig, ax = plt.subplots()
-    c = ax.contourf(x,y,contour_arr,levels=1000,cmap='jet')
-    plt.colorbar(c,label = 'Temperature ($ \degree$C)')
+    fig, ax = plt.subplots() 
+    c = ax.contourf(x,y,contour_arr,levels=1000,cmap='jet') #setting the contour/color of the plot
+    plt.colorbar(c,label = 'Temperature ($ \degree$C)') #add color bar to plot
     ax.set_xlabel('$x$-Position (cm)')
     ax.set_ylabel('$y$-Position (cm)')
     
 
     #SAVE CSV
-    arr = np.array(data)
-    df = pd.DataFrame(arr) # turn to dataframe
-    df.to_csv(str(t)+'.csv')
+    csv_arr = np.array(data) #store the data into an array for saving the csv file
+    df = pd.DataFrame(csv_arr) # turn to dataframe
+    df.to_csv(str(t)+'.csv') #
     plt.show(block = False)
     time.sleep(2)
     plt.close()
